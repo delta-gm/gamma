d6bc57baef6f14ef4d0cbcb3d02d83f6cbd2f767
Emi <nunya>
2021-03-15 20:18:19 -0500

    Finsihed formatting python files

--- fetch_market.py
+++ fetch_market.py
@@ -6,51 +6,30 @@ import statsmodels.api as sm
 import yfinance as yf
 import plotly.graph_objs as go
 from datetime import datetime
+import os
 
 # This should be used to initialize the R models. Each ticker will need AR, MA, ARMA, ARIMA models
 # compared for residuals and fit. Once the histories of all these tickers have been fitted in a model, 
 # the model should be fed data from fetch_day.py. 
 
-# dateObj = datetime.now()
-# dateStamp=str(dateObj.year)+'-'+'0'+str(dateObj.month)+'-'+str(dateObj.day)
-# print(dateStamp)
+PERIOD = '2y'
+INTERVAL = '1d'
 
-nxp_data=yf.download(tickers='NXPI',period='2y',interval='1d')
-nxp_data.to_csv(r'C:\repos\GammaWar\NXP_2years.csv',index=True)
+symbols = [
+    {'s':'NXPI','n':'NXP'},
+    {'s':'TSLA','n':'TESLA'},
+    {'s':'GME','n':'GAMESTOP'},
+    {'s':'AAPL','n':'APPLE'},
+    {'s':'PLTR','n':'PALANTIR'},
+    {'s':'NOK','n':'NOKIA'},
+    {'s':'BB','n':'BLACKBERRY'},
+    {'s':'ENPH','n':'ENPHASE'},
+    {'s':'SPWR','n':'SUNPOWER'},
+    {'s':'SUNW','n':'SUNWORKS'},
+    {'s':'SOL','n':'RENESOLA'}
+]
 
-gme_data=yf.download(tickers='GME',period='2y',interval='1d')
-gme_data.to_csv(r'C:\repos\GammaWar\GME_2years.csv',index=True)
-
-tesla_data=yf.download(tickers='TSLA',period='2y',interval='1d')
-tesla_data.to_csv(r'C:\repos\GammaWar\TSLA_2years.csv',index=True)
-
-apple_data=yf.download(tickers='AAPL',period='2y',interval='1d')
-apple_data.to_csv(r'C:\repos\GammaWar\AAPL_2years.csv',index=True)
-
-palantir_data=yf.download(tickers='PLTR',period='2y',interval='1d')
-palantir_data.to_csv(r'C:\repos\GammaWar\PLTR_2years.csv',index=True)
-
-nokia_data=yf.download(tickers='NOK',period='2y',interval='1d')
-nokia_data.to_csv(r'C:\repos\GammaWar\NOK_2years.csv',index=True)
-
-blackberry_data=yf.download(tickers='BB',period='2y',interval='1d')
-blackberry_data.to_csv(r'C:\repos\GammaWar\BB_2years.csv',index=True)
-
-enphase_data=yf.download(tickers='ENPH',period='2y',interval='1d')
-enphase_data.to_csv(r'C:\repos\GammaWar\ENPH_2years.csv',index=True)
-
-sunpower_data=yf.download(tickers='SPWR',period='2y',interval='1d')
-sunpower_data.to_csv(r'C:\repos\GammaWar\SPWR_2years.csv',index=True)
-
-sunworks_data=yf.download(tickers='SUNW',period='2y',interval='1d')
-sunworks_data.to_csv(r'C:\repos\GammaWar\SUNW_2years.csv',index=True)
-
-renesola_data=yf.download(tickers='SOL',period='2y',interval='1d')
-renesola_data.to_csv(r'C:\repos\GammaWar\SOL_2years.csv',index=True)
-
-
-
-print(NXP_data.head())
-print(enphase_data.head())
-print(renesola_data.head())
-
+for symbol in symbols:
+    filepath = os.getcwd() + '\\{}_{}.csv'.format(symbol['s'],PERIOD)
+    data = yf.download(tickers=symbol['s'],period=PERIOD,interval=INTERVAL)
+    data.to_csv(filepath,index=True)
